32b1e775f86afcc78fbf92220b66e615e7aff818
ain-3-21-413 <ain.3.21.413@gmail.com>
2023-04-27 12:24:43 +0600

    feat: make Button component to be able to react onClick

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,7 +5,7 @@ export default function Button(props) {
     const buttonStyles = `${styles.button} ${props.variant == 'fill' ? styles.fill : styles.outline}`;
 
     return (
-        <button class={buttonStyles}>
+        <button class={buttonStyles} onClick={props.onClick}>
             {props.children}
         </button>
     )
